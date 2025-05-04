932b383f07680627d0ccd89d2e5fe8155d26592f
hburn7 <hburnett777@gmail.com>
2025-05-04 15:39:02 -0400

    Update footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -1,22 +1,16 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import style from "./styles/footer.scss"
-import { version } from "../../package.json"
-import { i18n } from "../i18n"
 
 interface Options {
   links: Record<string, string>
 }
 
 export default ((opts?: Options) => {
-  const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
+  const Footer: QuartzComponent = ({ displayClass }: QuartzComponentProps) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
+        <p>osu! Tournament Rating © 2025</p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
